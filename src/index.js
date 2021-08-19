083d24f75039220b225ed0aab2694b20660e712b
Viktoriya Shpachynska <viktoriya.shpachynska@gmail.com>
2021-08-19 18:04:37 +0200

    Adds initial data

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import './sass/main.scss';
+
+// import '@pnotify/core/dist/PNotify.css';
+// import '@pnotify/desktop/dist/PNotifyDesktop';
+// import '@pnotify/core/dist/BrightTheme.css';
+
+// import { alert, notice, info, success, error } from '@pnotify/core';
+// import debounce from 'lodash.debounce';
+
+// const element = document.getElementById('.my-element-selector');
+// element.scrollIntoView({
+//   behavior: 'smooth',
+//   block: 'end',
+// });
